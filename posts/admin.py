3ce06c3bc1a7873747401ce68966b97cac05f64b
myra2004 <amirovamuqaddas@gmail.com>
2025-04-28 18:05:44 +0500

    posts

---
+++ posts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Post
+# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner__username', 'created_at', 'updated_at')
+    list_display_links = ("id", "title", "owner__username")
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('id', 'title', 'description')
+    readonly_fields = ('views_count',)
